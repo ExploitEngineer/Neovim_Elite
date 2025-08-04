c8330b41fc4534d2ba69e2f2c7596e86ca942a95
ExploitEngineer <revilwhoami0098@gmail.com>
2025-08-04 21:26:50 +0500

    feat(plugin): mini.nvim

---
+++ lua/plugins/mini.lua
@@ -0,0 +1,7 @@
+return {
+  {
+    "echasnovski/mini.nvim",
+    version = false, -- installs from `main`, as recommended
+    config = true, -- lazy.nvim will automatically call require("mini").setup()
+  },
+}
