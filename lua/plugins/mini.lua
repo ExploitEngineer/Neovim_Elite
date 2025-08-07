15bbb165ff81f6ffb5534a0536ef4896f5667863
ExploitEngineer <revilwhoami0098@gmail.com>
2025-08-07 23:18:13 +0500

    chore: remove mini.nvim plugin and related config

--- lua/plugins/mini.lua
+++
@@ -1,7 +0,0 @@
-return {
-  {
-    "echasnovski/mini.nvim",
-    version = false, -- installs from `main`, as recommended
-    config = true, -- lazy.nvim will automatically call require("mini").setup()
-  },
-}
